136cf34064e06cfa9bde2e6e905808478ce5e920
Igor Veselinovic <igor@veselinovic.net>
2020-03-31 23:27:13 -0400

    Fix some issues with SQL code

--- models/patch.go
+++ models/patch.go
@@ -10,26 +10,26 @@ import (
 type Patch struct {
 	Timestamp time.Time `json:"timestamp"`
 	Patch     string    `json:"patch"`
-	ConvoID   string    `json:"convo_id"`
-	UserID    string    `json:"user_id"`
+	ConvoID   int64     `json:"convo_id"`
+	UserID    int64     `json:"user_id"`
 	Type      string    `json:"type"`
 }
 
 type Filter struct {
-	Conversation string    `schema:"convo_id"`
-	User         []string  `schema:"user_id"`
+	Conversation int64     `schema:"convo_id"`
+	User         []int64   `schema:"user_id"`
 	Type         []string  `schema:"type"`
 	EndTime      time.Time `schema:"end_time"`
 	StartTime    time.Time `schema:"start_time"`
 }
 
-// Get patch rows from database using filters
+// GetPatches gets patch rows from the database using filters
 func (db *DB) GetPatches(filter *Filter) ([]Patch, error) {
 
 	// Create filter string
 	filterString := new(strings.Builder)
-	if filter.Conversation != "" {
-		fmt.Fprintf(filterString, "convo_id = %s", filter.Conversation)
+	if filter.Conversation != 0 {
+		fmt.Fprintf(filterString, "convo_id = %d", filter.Conversation)
 	} else {
 		log.Print("Error with conversation id")
 		return nil, nil
@@ -40,9 +40,9 @@ func (db *DB) GetPatches(filter *Filter) ([]Patch, error) {
 
 		for i, userID := range filter.User {
 			if i == 0 {
-				fmt.Fprintf(filterString, "user_id = %s", userID)
+				fmt.Fprintf(filterString, "user_id = %d", userID)
 			} else {
-				fmt.Fprintf(filterString, " OR user_id = %s", userID)
+				fmt.Fprintf(filterString, " OR user_id = %d", userID)
 			}
 		}
 
@@ -54,9 +54,9 @@ func (db *DB) GetPatches(filter *Filter) ([]Patch, error) {
 
 		for i, patchType := range filter.Type {
 			if i == 0 {
-				fmt.Fprintf(filterString, "type = %s", patchType)
+				fmt.Fprintf(filterString, "type = '%s'", patchType)
 			} else {
-				fmt.Fprintf(filterString, " OR type = %s", patchType)
+				fmt.Fprintf(filterString, " OR type = '%s'", patchType)
 			}
 		}
 
@@ -76,12 +76,12 @@ func (db *DB) GetPatches(filter *Filter) ([]Patch, error) {
 	queryString := new(strings.Builder)
 	fmt.Fprintf(queryString, "SELECT * FROM patches WHERE %s", filterString)
 	rows, err := db.Query(queryString.String())
-	defer rows.Close()
 	if err != nil {
 		log.Print("Error getting rows")
 		log.Print(err)
 		return nil, err
 	}
+	defer rows.Close()
 
 	p := Patch{}
 	patches := make([]Patch, 0)
@@ -98,7 +98,7 @@ func (db *DB) GetPatches(filter *Filter) ([]Patch, error) {
 	return patches, err
 }
 
-// Add new patch to the database
+// CreatePatch adds a new patch to the database
 func (db *DB) CreatePatch(patch *Patch) error {
 
 	// Insert patch into database
@@ -112,7 +112,7 @@ func (db *DB) CreatePatch(patch *Patch) error {
 	return nil
 }
 
-// Delete patches from the database by conversation
+// DeletePatches deletes patches from the database by conversation
 func (db *DB) DeletePatches(convo_id int64) (int64, error) {
 
 	// Delete patches from db
