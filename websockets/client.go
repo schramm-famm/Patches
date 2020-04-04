e1a5de84a9b95f5bd723b0373fa71f7ea58f5e57
Igor Veselinovic <igor@veselinovic.net>
2020-04-04 15:04:52 -0400

    Update WS logic to use timeouts properly
    Use ping-pong messages to see if the client is still alive, and use
    specific timeout values on reads and writes.

--- websockets/client.go
+++ websockets/client.go
@@ -3,8 +3,21 @@ package websockets
 import (
 	"log"
 	"patches/protocol"
+	"time"
 
+	"github.com/gorilla/websocket"
 	gorillaws "github.com/gorilla/websocket"
+)
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
 )
 
 // Client manages a WebSocket connection with a client.
@@ -45,6 +58,11 @@ func (c *Client) read() {
 		c.broker.unregister(c)
 	}()
 
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error {
+		c.conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
 	for {
 		_, data, err := c.conn.ReadMessage()
 		if err != nil {
@@ -61,18 +79,36 @@ func (c *Client) read() {
 // write sends messages to the WebSocket connection whenever new messages are
 // sent into the Client's channel.
 func (c *Client) write() {
+	ticker := time.NewTicker(pingPeriod)
 	defer func() {
-		c.conn.WriteMessage(
-			gorillaws.CloseMessage,
-			gorillaws.FormatCloseMessage(gorillaws.CloseGoingAway, "Going away"),
-		)
+		ticker.Stop()
+		c.conn.Close()
 	}()
 
-	for message := range c.send {
-		err := c.conn.WriteMessage(gorillaws.TextMessage, message)
-		if err != nil {
-			log.Print("Failed to write message to WebSocket: ", err)
-			return
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.conn.WriteMessage(
+					gorillaws.CloseMessage,
+					gorillaws.FormatCloseMessage(gorillaws.CloseGoingAway, "Going away"),
+				)
+				return
+			}
+
+			err := c.conn.WriteMessage(gorillaws.TextMessage, message)
+			if err != nil {
+				log.Print("Failed to write message to WebSocket: ", err)
+				return
+			}
+
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				log.Print("Failed to write ping message to WebSocket: ", err)
+				return
+			}
 		}
 	}
 }
